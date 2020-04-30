d94d4eb12a1aa53b1dea6b99f6396cb5ae4e05b5
dilip <dilipsb510@gmail.com>
2020-04-30 16:07:52 +0530

    Bug 00001: Bug heading 	Resolution : Division by zero 	changed made in function div

--- SourceCode/Maths/Numbers.c
+++ SourceCode/Maths/Numbers.c
@@ -13,6 +13,12 @@ int mul(int a,int b){
 	return a*b;
 }
 
+int div(int a,int b){
+	if(y!=0) return a/b;
+	printf("Division by zero\n");
+}
+}
+
 int main(){
 	for(int i=1;i<=15;i++){
 		printf("%d\n",i);
