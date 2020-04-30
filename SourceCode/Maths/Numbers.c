c30069c14fc324aa01628d8ba4392bac48eacebc
dilip <dilipsb510@gmail.com>
2020-04-30 12:55:31 +0530

    Added Functionallity 	added sum and sub function

--- SourceCode/Maths/Numbers.c
+++ SourceCode/Maths/Numbers.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 #include<math.h>
 
+int sum(int a,int b){
+	return a+b;
+}
+
+int sub(int a,int b){
+	return a-b;
+}
+
 int main(){
 	for(int i=1;i<=15;i++){
 		printf("%d\n",i);
